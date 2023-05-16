fa776399bfc32cb0b1143f527817e12d58b1d115
Emmanuel Peña <emmanuel.pena101@alu.ulpgc.es>
2023-05-16 18:28:50 +0000

    forgot password paripe

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -17,6 +17,12 @@ export class ForgotPasswordComponent {
               private router:Router) { }
 
   async onReset() {
+    try {
+      window.alert("Correo electrónico enviado, ¡comprueba tu bandeja de entrada!");
+      this.router.navigate(['/login']);
+    } catch(error) {
+      console.log(error);
+    }
     /*try {
       const email = this.userEmail.value!!;
       await this.resetPasswdSvc.resetPassword(email);
